db2610260d2df4be52fd78eb7a97047b5dcb57b3
Manuelrkocortes <meduardocortes@ucundinamarca.edu.co>
2025-02-28 17:50:26 -0500

    apuestas

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,69 @@
 import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Random;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //1 duplica
+        //2 pierde la mitad
+        //3 pierdetodo
         Scanner teclado = new Scanner(System.in);
-        double peso;
-        double altura;
-        double indice;
-        System.out.println("digite su peso");
-        peso = teclado.nextDouble();
-        System.out.println("digite su altura");
-        altura = teclado.nextDouble();
-        indice = peso/(altura*altura);
-        if (indice <18.5 ) {
-            System.out.println("Bajo Peso");
-        } else if (indice >= 18.6 && indice <24.9) {
-            System.out.println("Normal");
-        } else if (indice >=25  && indice <26.9 ) {
-            System.out.println("Sobrepeso grado1");
-        } else if (indice >=27  && indice <29.9 ) {
-            System.out.println("sobrepeso grado 2");
-        }else if (indice >= 30 && indice <34.9) {
-                System.out.println("Obesidad tipo 1");
-            } else if (indice >=35 && indice <39.9 ) {
-                System.out.println("obesidad tipo 2");
-            } else if (indice >=40  && indice <49.9) {
-                System.out.println("obesidad tipo 3 (morbida)");
-            } else if (indice >=50 ) {
-                System.out.println("obesidad tipo 4 (extrema)");
-        }
-        }
+        Random ale = new Random();
+        double dinero;
+        int aleatorio;
+        double resultado = 0;
+        int opcion;
+        aleatorio = ale.nextInt((3 - 1 + 1)) + 1;
+        System.out.println("digite la cantidad que quiere apostar");
+        dinero = teclado.nextInt();
+        do {
+            if (aleatorio == 3) {
+                resultado = dinero * 0;
+                System.out.println("ha perdido su dinero");
+                System.out.println("su dinero es:" + resultado);
+                break;
+            } else if (aleatorio == 2) {
+
+                resultado = dinero / 2;
+                System.out.println("ha perdido la mitad de su dinero");
+                System.out.println("quiere seguir jugando si=1 no=2");
+                opcion = teclado.nextInt();
+                if (opcion == 1) {
+                     aleatorio = ale.nextInt((3 - 1 + 1)) + 1;
+                    continue;
+                }
+                else if (opcion ==2){
+                    break;
+                }
+
+                }
+             else if (aleatorio == 1) {
+                resultado = dinero * 2;
+                System.out.println("su dinero se duplico");
+                System.out.println("su ganancia es:" + resultado);
+                System.out.println("quiere seguir jugando si=1 no=2");
+                opcion = teclado.nextInt();
+                if (opcion == 1) {
+                     aleatorio = ale.nextInt((3 - 1 + 1)) + 1;
+                    continue;
+                }
+                else if (opcion ==2){
+                    break;
+                }
+            }
+            System.out.println("su dinero se duplico");
+             System.out.println("su ganancia es:" + resultado);
+            System.out.println("quiere seguir jugando si=1 no=2");
+            opcion = teclado.nextInt();
+            if (opcion == 1) {
+                continue;
+            }
+            else if (opcion ==2){
+                break;
+            }
+        }while ( aleatorio == ale.nextInt((3 - 1 + 1)) + 1);
+
     }
+}
